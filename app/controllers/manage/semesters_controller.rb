e433af70096b7491108d4834d1983151e19a8fce
Evgeniy Serykh <sev4252@gmail.com>
2010-12-17 16:47:33 +0600

    #6051. CRU: семестры

--- app/controllers/manage/semesters_controller.rb
+++ app/controllers/manage/semesters_controller.rb
@@ -1,6 +1,8 @@
 class Manage::SemestersController < Manage::ApplicationController
   inherit_resources
-  defaults :resource_class => Speciality::Semester
+
+  defaults :resource_class => Speciality::Semester,
+           :instance_name => 'speciality_semester'
 
   belongs_to :chair, :shallow => true do
     belongs_to :speciality
@@ -9,10 +11,6 @@ class Manage::SemestersController < Manage::ApplicationController
   def create
     create!(:location => parent_path(parent))
   end
-
-#  def edit
-#    raise resource_instance_name.inspect
-#  end
 
   def update
     update!(:location => parent_path(parent))
