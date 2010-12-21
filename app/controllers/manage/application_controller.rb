c1e45c2fbf7cc668a7b8f6c957795234522172a7
Manin Iliy <bkmzvf@gmail.com>
2010-12-21 15:26:53 +0600

    Не пускаем анонима в манаж

--- app/controllers/manage/application_controller.rb
+++ app/controllers/manage/application_controller.rb
@@ -4,5 +4,12 @@ class Manage::ApplicationController < ApplicationController
 
   custom_actions :resource => :delete
 
+  before_filter :verify_admin
+
+  private
+  def verify_admin
+    redirect_to root_url unless current_user && current_user.roles.include?(:admin)
+  end
+
 end
 
