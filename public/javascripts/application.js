0e573dc8738eacf0bb96b52216a214d9fbae2a0f
Evgeny Lapin <lev@openteam.ru>
2010-12-22 11:01:10 +0600

    #6122. GUI: flash notice/alert

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,15 @@
+function flash() {
+  $("#error_explanation").addClass("flash_block").prepend("<a href='#'>закрыть</a>");
+  $(".flash_block a").click(function(e) {
+    e.preventDefault();
+    $(this).parent().fadeOut(function() {
+      $(this).remove();
+    });
+  });
+};
+
 $(function() {
+  flash();
   $('a[rel=tipsy]').tipsy({
     gravity: 's'
   });
