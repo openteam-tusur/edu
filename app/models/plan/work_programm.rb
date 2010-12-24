4326e87880cd35d4dca1a018599fc4c62356c662
Dmitriy Baraksanov <bdn@openteam.ru>
2010-12-24 10:37:52 +0600

    #6165. модель рабочей программы. удалил бажную модель

--- app/models/plan/work_programm.rb
+++
@@ -1,8 +0,0 @@
-class Plan::WorkProgramm < ActiveRecord::Base
-  belongs_to :education, :class_name => "Plan::Education"
-  belongs_to :human
-
-  validates_presence_of :year, :human, :access, :resource_name
-  validates_numericality_of :year, :only_integer => true
-
-end
