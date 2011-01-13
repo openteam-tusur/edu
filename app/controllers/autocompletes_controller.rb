e32b6c19a6672968aeb5c6670bd8fce1f9a2fe00
Dmitry Lihachev <lda@openteam.ru>
2011-01-13 16:35:05 +0600

    #6255. Автокомплит дисциплины в учебном плане. Автокомплит ч/з solr

--- app/controllers/autocompletes_controller.rb
+++ app/controllers/autocompletes_controller.rb
@@ -3,7 +3,11 @@
 class AutocompletesController < ActionController::Base
 
   def disciplines
-    disciplines = Plan::Discipline.all
+    disciplines = Plan::Discipline.solr_search do
+      text_fields do
+        with(:name).starting_with params[:term]
+      end
+    end.results
     disciplines.map! do | discipline |
       { :id => discipline.id, :value => discipline.name }
     end
