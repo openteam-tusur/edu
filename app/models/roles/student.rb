f449f30352626e5e3e5c05b4ea196d8f3efe6f68
Evgeniy Serykh <sev4252@gmail.com>
2011-01-14 11:52:58 +0600

    список сотрудников кафедры

--- app/models/roles/student.rb
+++ app/models/roles/student.rb
@@ -4,7 +4,7 @@ class Roles::Student < Role
   validates_presence_of :group, :birthday
   validates_uniqueness_of :group, :scope => [:human_id, :state]
 
-  default_values :title => 'Студент', :slug => 'student'
+  default_values :title => 'Студент', :slug => 'student', :post => 'Студент'
 end
 
 
