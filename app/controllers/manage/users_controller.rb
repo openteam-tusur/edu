60d78c6bf46cb24bbd5f18be0f8dd59326c5bf54
Manin Iliy <bkmzvf@gmail.com>
2011-01-21 09:45:36 +0600

    #6338. Просмотр пользователя, редактирование e-mail

--- app/controllers/manage/users_controller.rb
+++ app/controllers/manage/users_controller.rb
@@ -1,2 +1,6 @@
 class Manage::UsersController < Manage::ApplicationController
+  load_and_authorize_resource
+  def update
+    update! { redirect_to manage_human_path(@user.human) and return}
+  end
 end
