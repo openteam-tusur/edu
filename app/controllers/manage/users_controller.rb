a547e3d3fa8b2882711a4b8caebb474066a83c33
Manin Iliy <bkmzvf@gmail.com>
2011-01-24 10:53:00 +0600

    Сброс пароля

--- app/controllers/manage/users_controller.rb
+++ app/controllers/manage/users_controller.rb
@@ -1,8 +1,14 @@
 class Manage::UsersController < Manage::ApplicationController
   load_and_authorize_resource
+  belongs_to :human, :singleton => true
+  actions :edit, :update
+  custom_actions :resource => :flush_password
 
-  def update
-    update! { manage_human_path(@user.human) }
+  def flush_password
+    flush_password! do
+      @user.send_reset_password_instructions
+      redirect_to parent_path and return flash[:notice] = t("devise.passwords.send_instructions_from_admin")
+    end
   end
 
 end
