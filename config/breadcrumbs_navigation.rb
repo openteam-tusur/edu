f3ac7cad6465a0b6c73663c78d9066522709ba1e
Nikeshkin Lev <ARnikev@mail.ru>
2011-01-25 12:31:16 +0600

    добавлены крошки для просмотра дисциплин

--- config/breadcrumbs_navigation.rb
+++ config/breadcrumbs_navigation.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
+
   primary.item :chairs, t("title.chairs.index"), chairs_path, :class => "subfaculties",
                   :highlights_on => /^\/chairs|^\/employees/ do |user|
 
@@ -17,10 +18,17 @@ SimpleNavigation::Configuration.run do |navigation|
                             @speciality.title,
                             chair_speciality_path(@chair, @speciality) do |speciality|
 
-        # учебные планы
+           # учебные план
             speciality.item :curriculum,
                             @curriculum.title,
-                            chair_speciality_curriculum_path(@chair, @speciality, @curriculum) if @curriculum
+                            chair_speciality_curriculum_path(@chair, @speciality, @curriculum) do |curriculum|
+              # семестр
+              curriculum.item :semester,
+                              @semester.title,
+                              chair_speciality_curriculum_semester_path(@chair, @speciality, @curriculum, @semester) do |semester|
+                semester.item :education, @education.discipline.name, "#" if @education
+              end
+            end if @curriculum
           end if @speciality
         end
       end if @chair
