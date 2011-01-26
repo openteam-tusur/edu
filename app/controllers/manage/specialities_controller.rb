7e5b3ac3e0e3c72b8014fcf56677d270af930fa4
Manin Iliy <bkmzvf@gmail.com>
2011-01-26 17:56:02 +0600

    #6484. CRUD: специальности

--- app/controllers/manage/specialities_controller.rb
+++ app/controllers/manage/specialities_controller.rb
@@ -1,13 +1,8 @@
 class Manage::SpecialitiesController < Manage::ApplicationController
-
   load_and_authorize_resource
 
   defaults :finder => :find_by_slug
-  belongs_to :chair, :finder => :find_by_slug
 
-  def destroy
-    destroy!{ parent_path }
-  end
-
+  actions :all, :except => [:show]
 end
 
