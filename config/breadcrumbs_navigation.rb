ddc4173bfa56da7a55d319bee7c180073c3df6ae
Evgeniy Serykh <sev4252@gmail.com>
2011-02-07 09:25:35 +0600

    немножно крошек

--- config/breadcrumbs_navigation.rb
+++ config/breadcrumbs_navigation.rb
@@ -2,37 +2,46 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
 
-  primary.item :chairs, t("title.chairs.index"), chairs_path, :class => "subfaculties",
-                  :highlights_on => /^\/chairs|^\/employees/ do |user|
+  primary.item :humans,
+               t('title.humans.index'),
+               humans_path,
+               :highlights_on => /^\/humans/
 
-      user.item "chair_#{@chair.slug}", @chair.abbr,
+    primary.item :chairs,
+                 t("title.chairs.index"),
+                 chairs_path,
+                 :class => "subfaculties",
+                 :highlights_on => /^\/chairs|^\/employees/ do |user|
+
+        user.item "chair_#{@chair.slug}",
+                  @chair.abbr,
                   chair_path(@chair) do |chair|
 
-        # список специальностей
-        chair.item :specialities,
-                   t("title.specialities.index"),
-                   chair_specialities_path(@chair) do |specialities|
+          # список специальностей
+          chair.item :specialities,
+                     t("title.specialities.index"),
+                     chair_specialities_path(@chair) do |specialities|
 
-          # специальности
-          specialities.item :speciality,
-                            @speciality.title,
-                            chair_speciality_path(@chair, @speciality) do |speciality|
+            # специальности
+            specialities.item :speciality,
+                              @speciality.title,
+                              chair_speciality_path(@chair, @speciality) do |speciality|
 
-           # учебные план
-            speciality.item :curriculum,
-                            @curriculum.title,
-                            chair_speciality_curriculum_path(@chair, @speciality, @curriculum) do |curriculum|
-              # семестр
-              curriculum.item :semester,
-                              @semester.title,
-                              chair_speciality_curriculum_semester_path(@chair, @speciality, @curriculum, @semester) do |semester|
-                semester.item :education, @education.discipline.name, "#" if @education
-              end
-            end if @curriculum
-          end if @speciality
-        end
-      end if @chair
+             # учебные план
+              speciality.item :curriculum,
+                              @curriculum.title,
+                              chair_speciality_curriculum_path(@chair, @speciality, @curriculum) do |curriculum|
+                # семестр
+                curriculum.item :semester,
+                                @semester.title,
+                                chair_speciality_curriculum_semester_path(@chair, @speciality, @curriculum, @semester) do |semester|
+                  semester.item :education, @education.discipline.name, "#" if @education
+                end
+              end if @curriculum
+            end if @speciality
+          end
+        end if @chair
+      end
     end
-  end
 end
 
