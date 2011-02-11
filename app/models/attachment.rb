9b42c72bb166727f0d1b042bd8ef4c272b7043aa
Dmitriy Baraksanov <bdn@openteam.ru>
2011-02-11 15:14:38 +0600

    #6636. не создаются публикации, что-то с mime_type

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -19,7 +19,7 @@ class Attachment < ActiveRecord::Base
 
   def set_mime_type
     return unless self.data.file? && !data.queued_for_write[:original].nil?
-    self.data_content_type =  MIME::Types[FileMagic.new(FileMagic::MAGIC_MIME).file(data.queued_for_write[:original].path)].first
+    self.data_content_type =  MIME::Types[FileMagic.new(FileMagic::MAGIC_MIME).file(data.queued_for_write[:original].path)].first.content_type
   end
 
   def set_hash
