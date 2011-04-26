9f49dbf91710d8c40d12d24347a7a49cf952442d
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-04-26 15:14:55 +0700

    change one character in secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Portal::Application.config.secret_token = 'de3b847b6019df906a9d827cde8124a7e0732d236f69d7dcbe03e8102bd529e7eb7e16603af4c24d718816dc0637bf77de9c1909ee037d13586dceff73414ed0'
+Portal::Application.config.secret_token = '5e3b847b6019df906a9d827cde8124a7e0732d236f69d7dcbe03e8102bd529e7eb7e16603af4c24d718816dc0637bf77de9c1909ee037d13586dceff73414ed0'
