cc7f2b0ea65ebec7f1f049bd057907d108ec99b4
Evgeny Lapin <lev@openteam.ru>
2011-06-23 10:49:49 +0700

    ещё один факультет и кафедра

--- db/schema.rb
+++ db/schema.rb
@@ -207,6 +207,7 @@ ActiveRecord::Schema.define(:version => 20110429093600) do
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
