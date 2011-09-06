15dbd3097454872deef5fa28057b90eab5c5d772
Evgeny Lapin <lev@openteam.ru>
2011-09-06 10:04:56 +0700

    Текстовки для заполнения профиля и выбора роли

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,7 @@ module Portal
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    config.log_level = :warn
+    config.log_level = :info
   end
 end
 
--- lib/development_imperian_mail_interceptor.rb
+++ lib/development_imperian_mail_interceptor.rb
@@ -2,7 +2,7 @@ class DevelopmentImperianMailInterceptor
 
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = SETTINGS['mailer']['admin_email']
+    message.to = Settings['mailer']['admin_email']
   end
 
 end
