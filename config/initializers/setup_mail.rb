f30b0dd1cd4c782d8b93d19476b45769dbe25d1f
Evgeny Lapin <lev@openteam.ru>
2011-09-08 10:45:30 +0700

    правильный мерж smtp_settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,5 @@
 mailer = Rails.configuration.action_mailer
 mailer.register_interceptor(DevelopmentImperianMailInterceptor) if Settings['mailer.send_all_mail_to'].present?
 mailer.default_url_options = { :host => Settings[:domain] }
-mailer.smtp_settings = Settings['mailer.smtp_settings'].merge(:domain => Settings['domain'])
+mailer.smtp_settings = {:domain => Settings['domain']}.merge(Settings['mailer.smtp_settings'] || {})
+
