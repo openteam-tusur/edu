523587ee46728a74698c269dc8ca1d76e0ef33fb
Dmitry Lihachev <lda@openteam.ru>
2011-09-08 09:57:34 +0700

    ensure notification_from is set

--- config/initializers/_read_settings.rb
+++ config/initializers/_read_settings.rb
@@ -3,10 +3,11 @@ Settings.read(Rails.root.join('config', 'settings.yml'))
 Settings.defaults Settings.extract!(Rails.env)[Rails.env] || {}
 Settings.extract!(:test, :development, :production)
 
-Settings.define 'domain',                       :required => Rails.env.production?
-Settings.define 'mailer.send_notifcations_to',  :required => true
-Settings.define 'students.host',                :required => Rails.env.production?
-Settings.define 'hoptoad.api_key',              :required => Rails.env.production?
-Settings.define 'hoptoad.host',                 :required => Rails.env.production?
+Settings.define 'domain',                         :required => true
+Settings.define 'mailer.send_notifcations_from',  :required => true
+Settings.define 'mailer.send_notifcations_to',    :required => true
+Settings.define 'students.host',                  :required => Rails.env.production?
+Settings.define 'hoptoad.api_key',                :required => Rails.env.production?
+Settings.define 'hoptoad.host',                   :required => Rails.env.production?
 
 Settings.resolve!
