6ffb8c23c599747f3716648a564248e4b24152f3
Dmitry Gmyzin <gmyzind@gmail.com>
2011-11-16 13:16:47 +0700

    Checbox по тематикам

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -7,19 +7,12 @@ class RecordsController < CrudController
 
   def index
     @main_subjects = MainSubject.all(:order => 'title')
-    @main_subject = MainSubject.find_by_id(params[:main_subject]) if params[:main_subject]
-    @subject = Subject.find_by_id(params[:subject], :order => 'title') if params[:subject]
-    
     @years = Year.all(:order => 'title DESC')
-    @year = Year.find_by_id(params[:year]) if params[:year]
-    @month = Month.find_by_id(params[:month]) if params[:month]
+    
+    params[:subject] ||= []
   
     @search = Record.search(params)
     @records = @search.results
-    @main_subject_counts = Record.main_subject_counts(params)
-    @subject_counts = Record.subject_counts(params) if params[:main_subject]
-    @year_counts = Record.year_counts(params)
-    @month_counts = Record.month_counts(params) if params[:year]
   end
 
   def show
