2800e5224385421e42f66d6728ad2443db45577f
Dmitry Lihachev <lda@openteam.ru>
2013-04-03 10:40:37 +0700

    students.host => students.url

--- config/initializers/_read_settings.rb
+++ config/initializers/_read_settings.rb
@@ -6,7 +6,7 @@ Settings.extract!(:test, :development, :production)
 Settings.define 'domain',                         :required => true
 Settings.define 'mailer.send_notifications_from', :required => true
 Settings.define 'mailer.send_notifications_to',   :required => true
-Settings.define 'students.host',                  :required => Rails.env.production?
+Settings.define 'students.url',                   :required => Rails.env.production?
 Settings.define 'hoptoad.api_key',                :required => Rails.env.production?
 Settings.define 'hoptoad.host',                   :required => Rails.env.production?
 
